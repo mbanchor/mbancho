4bbe15125e5212512d2fd63c797ebdd4a8cefb6f
rick237518 <67753269+rick237518@users.noreply.github.com>
2020-10-17 02:52:35 +0200

    Updates

---
+++ precache-manifest.6e0881502d4bfe4333d61ba3592f4ecd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5795e3ccc0e3a0d6d1a0dc9d58cf33e",
+    "url": "/mbancho/index.html"
+  },
+  {
+    "revision": "115d641881032bb7a6d2",
+    "url": "/mbancho/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f78e55b74f82e0259369",
+    "url": "/mbancho/static/js/2.7e58f0f5.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/mbancho/static/js/2.7e58f0f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "115d641881032bb7a6d2",
+    "url": "/mbancho/static/js/main.93ab86c5.chunk.js"
+  },
+  {
+    "revision": "4f3d743c60be85bd8454",
+    "url": "/mbancho/static/js/runtime-main.a464daa3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mbancho/static/media/logo.5d5d9eef.svg"
+  }
+]);
